691c6ca438e2d6c477d4cb122b54409b57771a1b
Brian Grimshaw <brian@madideas.net>
2026-08-28 22:15:01 +0100

    Licence added

--- FX_Control.py
+++ FX_Control.py
@@ -1,4 +1,19 @@
-# Use USB GPIO to switch FX on and off
+# Use USB GPIO to switch Reaper FX on and off
+# Copyright (C) 2026  Brian Grimshaw
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses
+
 # Bits 0 to 4 are LED outputs
 # Bits 5 to 7 are momentary pushbutton inputs
 # On init, try to open serial port
